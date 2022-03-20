d9e44ac76cd885378347da5aefd2b0e53aa9ef99
Steven <carlos.lopez-mo@uniminuto.edu.co>
2022-03-19 21:28:09 -0500

    Objeto Math principales :)

---
+++ objetoMath.js
@@ -0,0 +1,18 @@
+/*
+          Objeto math objeto estatico simepre hay que invocarlo uwu
+          https://www.youtube.com/watch?v=BGXwssmxGuY&list=PLvq-jIkSeTUZ6QgYYO3MwG9EMqC-KoLXA&index=30
+          Curso JavaScript: 28. Objeto Math - #jonmircha
+          */
+
+console.log(Math);
+console.log(Math.abs(7.834)); //trae el valor absoluto
+console.log(Math.ceil(7.1)); // redondea al numero entero mayor dara 8
+console.log(Math.floor(7.9)); // redondea al numero entero menor dara 7
+console.log(Math.round(5.4)); //redondea con justicia sin bajar o subir dependiendo este es mas legal
+console.log(Math.sqrt(5)); // saca la raiz cuadrada
+console.log(Math.pow(2, 5)); //saca las potencias
+console.log(Math.sign(0.48)); // dice si es mayor, negativo o cero ("negativo -1" "cero 0" "entero 1")
+console.log(Math.random()); //da un valor aleatorio
+console.log(Math.random() * 10); // trae numero aleatorio del 1 al 10
+//Crea un programa que me arroje un numero aleatorio del 1 al 50 enteros
+console.log(Math.round(Math.random() * 50));
